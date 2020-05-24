fc39052bc4b4f05c6266f4d1396247141a942793
Dmitry Serebryakov <dmserebr@gmail.com>
2020-05-24 17:49:35 +0300

    Monitor disk space usage

--- src/tasks.py
+++ src/tasks.py
@@ -5,7 +5,7 @@ import config
 import netboxz
 import models
 import memstat
-
+import subprocess
 
 last_temp = 0
 
@@ -37,3 +37,21 @@ async def check_swap_usage(app):
                 app['bot'].send_message(chat.id, message)
 
         await asyncio.sleep(config.mem_update_time)
+
+async def check_disk_usage(app):
+    base_folder = config.disk_usage_monitor_folder
+    while True:
+        data = subprocess.check_output(['du', '--max-depth=1', base_folder]).decode('utf-8').split('\n')
+        stats = dict(map(lambda s: (s.split('\t')[1].replace(base_folder, ''), int(s.split('\t')[0])), [x for x in data if x != '']))
+        if (stats[''] > config.disk_usage_threshold):
+            message = 'Running out of space in /share: {0:0.3f}G [{1} bytes] used'.format(stats[''] / (1024 * 1024 * 1024), stats[''])
+            message += '\nTop users:'
+            max_len = max(list(map(lambda x: len(x), stats.keys())))
+            for k, v in sorted(stats.items(), key=lambda x: x[1], reverse=True):
+                if k != '':
+                    message += '\n{0}  {1:0.3f}G'.format(k.ljust(max_len, ' '), v / (1024 * 1024 * 1024))
+
+            for chat in models.Chat.select().where(models.Chat.user == None):
+                app['bot'].send_message(chat.id, message)
+
+        await asyncio.sleep(config.disk_usage_update_time)
